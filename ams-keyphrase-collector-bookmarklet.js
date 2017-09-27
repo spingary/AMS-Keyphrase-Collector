8b81bbb70f330a093f8dd9e127a2ec77e594b3c6
Gary Wong <gary.wong@spingroup.com>
2017-09-26 22:44:07 -0500

    Update URLs

--- ams-keyphrase-collector-bookmarklet.js
+++ ams-keyphrase-collector-bookmarklet.js
@@ -1,6 +1,7 @@
 javascript:(function(){
   var cpCode = document.createElement('script');cpCode.setAttribute('src', 'https://cdn.rawgit.com/zenorocha/clipboard.js/v1.7.1/dist/clipboard.min.js');document.body.appendChild(cpCode);
-  var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://rawgit.com/spingary/c1b85e7a7c170652cb07435d283c9a46/raw/7f7c04a42b8efc937d850d1904e88d343f469ca5/amazon-scraper.js');document.body.appendChild(jsCode);
+  var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://rawgit.com/spingary/AMS-Keyphrase-Collector/master/ams-keyphrase-collector.js');document.body.appendChild(jsCode);
 }());
-//Production: https://cdn.rawgit.com/spingary/c1b85e7a7c170652cb07435d283c9a46/raw/738d459dca6095917435a24a1cb3f57ab2d406b9/amazon-scraper.js
-//Dev: https://rawgit.com/spingary/c1b85e7a7c170652cb07435d283c9a46/raw/738d459dca6095917435a24a1cb3f57ab2d406b9/amazon-scraper.js
+//Production: https://cdn.rawgit.com/spingary/AMS-Keyphrase-Collector/1.0/ams-keyphrase-collector.js
+//Dev: https://rawgit.com/spingary/AMS-Keyphrase-Collector/1.0/ams-keyphrase-collector.js
+//https://rawgit.com/spingary/AMS-Keyphrase-Collector/master/ams-keyphrase-collector.js
